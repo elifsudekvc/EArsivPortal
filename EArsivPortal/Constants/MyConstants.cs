7042fa8c218169270294abd7c2ca582c7f6fc929
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-06-05 17:36:38 +0300

    add identity

---
+++ EArsivPortal/Constants/MyConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace EArsivPortal.Constants
+{
+    public enum Roles
+    {
+        Admin,
+        User
+    }
+    public class MyConstants
+    {
+    }
+}
---
+++ EArsivPortal/Data/Document.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EArsivPortal.Data
+{
+    [Table("Document")]
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        [Required]
+        public string DocumentName { get; set; }
+    }
+}
